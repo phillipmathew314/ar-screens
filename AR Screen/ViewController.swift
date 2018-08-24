c02aa6d68647543b5aae4ccf7f04a380b15f079e
Phillip Mathew <phillipmathew314@gmail.com>
2018-08-23 20:31:18 -0700

    Initial commit

---
+++ AR Screen/ViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  Youtube Screen
+//
+//  Created by Phillip Mathew on 8/23/18.
+//  Copyright © 2018 Phillip Mathew. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        //sceneView.scene = scene
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
+        self.registerGestureRecognizers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    private func registerGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        guard let currentFrame = self.sceneView.session.currentFrame else {
+            return
+        }
+        
+        let videoNode = SKVideoNode(fileNamed: "SampleVideo_1280x720_1mb.mp4")
+        videoNode.play()
+        
+        let skScene = SKScene(size: CGSize(width: 640, height: 480))    //skvideonodes can only be added to skscenes
+        skScene.addChild(videoNode)
+        
+        videoNode.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
+        videoNode.size = skScene.size
+        
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = skScene
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        //var translation = matrix_identity_float4x4
+        //translation.columns.3.z = -1.0
+        
+        //tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation) //translates the video 1 meter away
+        //tvPlaneNode.position = SCNVector3(0, 0, -2)
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let curPosCam = orientation + location
+        
+        tvPlaneNode.position = curPosCam
+        
+        tvPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)   //to make the video not upside down
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+        
+    }
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z);
+}
